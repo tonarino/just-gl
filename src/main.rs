7a86441758ad9df4fc058d085d450e93cf70f716
Brian Schwind <brianmschwind@gmail.com>
2023-09-08 15:44:15 +0900

    Fetch the first connected connector and print its preferred video mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use drm::{control::Device as ControlDevice, Device};
+use drm::{
+    control::{
+        connector::{Handle as ConnectorHandle, Info as ConnectorInfo, State as ConnectorState},
+        Device as ControlDevice, Mode, ModeTypeFlags, ResourceHandles,
+    },
+    Device,
+};
 use gbm::Device as GbmDevice;
 use std::os::fd::{AsFd, BorrowedFd};
 
@@ -27,14 +33,8 @@ impl Card {
     }
 }
 
-fn main() {
-    // TODO(bschwind) - Use libdrm to iterate over available DRM devices.
-    let gpu = Card::open("/dev/dri/card0");
-    dbg!(gpu.get_driver().expect("Failed to get GPU driver info"));
-    dbg!(gpu.get_bus_id().expect("Failed to get GPU bus ID"));
-
-    let resources = gpu.resource_handles().expect("Failed to get GPU resource handles");
-    dbg!(&resources);
+fn print_connector_info(gpu: &Card, resources: &ResourceHandles) {
+    println!("Connectors:");
 
     for connector_handle in &resources.connectors {
         let force_probe = false;
@@ -42,8 +42,72 @@ fn main() {
             .get_connector(*connector_handle, force_probe)
             .expect("Failed to get GPU connector info");
 
-        dbg!(connector);
+        let connector_interface = connector.interface().as_str();
+        let interface_id = connector.interface_id();
+
+        let connection_state = match connector.state() {
+            ConnectorState::Connected => "✅",
+            ConnectorState::Disconnected => "❌",
+            ConnectorState::Unknown => "❔",
+        };
+
+        println!("\t{connector_interface}-{interface_id}, Connected={connection_state}");
     }
+}
+
+fn first_connected_connector(gpu: &Card) -> Option<ConnectorHandle> {
+    gpu.resource_handles()
+        .expect("Failed to get GPU resource handles")
+        .connectors
+        .iter()
+        .find(|connector_handle| {
+            let force_probe = false;
+            gpu.get_connector(**connector_handle, force_probe)
+                .expect("Failed to get GPU connector info")
+                .state()
+                == ConnectorState::Connected
+        })
+        .copied()
+}
+
+fn connector_preferred_mode(connector_info: &ConnectorInfo) -> Option<Mode> {
+    connector_info
+        .modes()
+        .iter()
+        .find(|mode| mode.mode_type().contains(ModeTypeFlags::PREFERRED))
+        .copied()
+}
+
+fn main() {
+    // TODO(bschwind) - Use libdrm to iterate over available DRM devices.
+    let gpu = Card::open("/dev/dri/card0");
+    dbg!(gpu.get_driver().expect("Failed to get GPU driver info"));
+    dbg!(gpu.get_bus_id().expect("Failed to get GPU bus ID"));
+
+    let resources = gpu.resource_handles().expect("Failed to get GPU resource handles");
+
+    print_connector_info(&gpu, &resources);
+
+    let force_probe = false;
+    let Some(first_handle) = first_connected_connector(&gpu) else {
+        println!("No display connected, exiting");
+        return;
+    };
+
+    let first_connector =
+        gpu.get_connector(first_handle, force_probe).expect("Failed to get GPU connector info");
+
+    let connector_interface = first_connector.interface().as_str();
+    let interface_id = first_connector.interface_id();
+
+    println!("Using connector: {connector_interface}-{interface_id}");
+
+    let Some(preferred_mode) = connector_preferred_mode(&first_connector) else {
+        println!("No preferred mode for the first connected connector, exiting");
+        return;
+    };
+
+    println!("Using mode: {preferred_mode:?}");
 
     let _gbm = GbmDevice::new(gpu).expect("Failed to create GbmDevice");
 }
