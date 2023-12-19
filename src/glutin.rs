f27d0b30e9afcd135e0104363983624f89e95658
Marek Bernat <marek.bernat@gmail.com>
2023-12-19 17:51:41 +0100

    Render a frame; polish code a bit

--- src/glutin.rs
+++ src/glutin.rs
@@ -1,60 +1,49 @@
+use glium::backend::glutin::Display as GliumDisplay;
+use glutin::{
+    config::{ConfigSurfaceTypes, ConfigTemplateBuilder},
+    context::{ContextAttributesBuilder, NotCurrentGlContextSurfaceAccessor},
+    display::{GetGlDisplay, GlDisplay},
+    surface::{SurfaceAttributesBuilder, WindowSurface},
+};
+use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle};
 use std::num::NonZeroU32;
 
-use glutin::{context::NotCurrentGlContextSurfaceAccessor, display::GetGlDisplay};
-use raw_window_handle::HasRawWindowHandle;
-
-pub(crate) fn init(
-    window: &crate::Window,
-) -> glium::backend::glutin::Display<glium::glutin::surface::WindowSurface> {
-    use glutin::config::GlConfig;
-    use glutin::display::GlDisplay;
-    use glutin::{
-        api::egl::display::Display,
-        config::{ConfigSurfaceTypes, ConfigTemplateBuilder},
-    };
-    use raw_window_handle::HasRawDisplayHandle;
-
+// TODO(mbernat): SAFETY
+// NOTE(mbernat): This is a Frankestein glutin/glium init function that I created because it's the easiest
+// approach I found, not being familiar with either create. We should probably get rid of the glium bits here.
+pub(crate) fn init(window: &crate::Window) -> GliumDisplay<WindowSurface> {
     let display_handle = window.raw_display_handle();
-    let display = unsafe { Display::new(display_handle) }.expect("Failed to create glutin display");
+    let display = unsafe {
+        glutin::display::Display::new(display_handle, glutin::display::DisplayApiPreference::Egl)
+    }
+    .expect("Could not create glutin display");
 
-    let config_template = ConfigTemplateBuilder::default()
-        .with_alpha_size(8)
-        .with_surface_type(ConfigSurfaceTypes::WINDOW)
-        .build();
+    // TODO(mbernat): make sure we're using the right config
+    let config_template =
+        ConfigTemplateBuilder::default().with_surface_type(ConfigSurfaceTypes::WINDOW).build();
 
     let config = unsafe { display.find_configs(config_template) }
         .unwrap()
-        .reduce(|config, acc| if config.num_samples() > acc.num_samples() { config } else { acc })
+        .next()
         .expect("No available configs");
 
-    println!("Picked a config with {} samples", config.num_samples());
-
-    // TODO(mbernat): get size from window
-    // let (width, height): (u32, u32) = window.inner_size().into();
-    let (width, height) = (1920, 1080);
-    let attrs = glutin::surface::SurfaceAttributesBuilder::<glutin::surface::WindowSurface>::new()
-        .build(
-            window.raw_window_handle(),
-            NonZeroU32::new(width).unwrap(),
-            NonZeroU32::new(height).unwrap(),
-        );
+    let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+        window.raw_window_handle(),
+        NonZeroU32::new(window.drm_display.width).unwrap(),
+        NonZeroU32::new(window.drm_display.height).unwrap(),
+    );
 
     // Finally we can create a Surface, use it to make a PossiblyCurrentContext and create the glium Display
-    // TODO(mbernat): make sure we're using the right config, this one comes from non-surface glutin example
     let surface = unsafe { config.display().create_window_surface(&config, &attrs).unwrap() };
     // NOTE(mbernat): None window handle should be fine for GBM
-    let context_attributes = glutin::context::ContextAttributesBuilder::new().build(None);
-    let current_context = (unsafe {
+    let context_attributes = ContextAttributesBuilder::new().build(None);
+    let no_context = unsafe {
         config
             .display()
             .create_context(&config, &context_attributes)
             .expect("failed to create context")
-    })
-    .make_current(&surface)
-    .unwrap();
-    glium::backend::glutin::Display::from_context_surface(
-        glutin::context::PossiblyCurrentContext::Egl(current_context),
-        glutin::surface::Surface::Egl(surface),
-    )
-    .unwrap()
+    };
+
+    let current_context = no_context.make_current(&surface).unwrap();
+    GliumDisplay::from_context_surface(current_context, surface).unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use clap::Parser;
-use drm::{control::Device as ControlDevice, Device};
-use gbm::{BufferObjectFlags, Device as GbmDevice, Format as BufferFormat};
+use drm::{
+    control::{
+        connector::Info as ConnectorInfo, crtc::Info as CrtcInfo, Device as ControlDevice, Mode,
+    },
+    Device,
+};
+use gbm::{BufferObject, Device as GbmDevice, Format as BufferFormat, Modifier, Surface};
 use just_gl::{
     connector_preferred_mode, first_encoder, get_connected_connectors, get_connector_name,
     print_connector_info, Card,
 };
-use raw_window_handle::HasRawDisplayHandle;
 use std::path::PathBuf;
-use std::{
-    ffi::c_void,
-    os::fd::{AsFd, BorrowedFd},
-    path::{Path},
-};
 
 mod glutin;
 
@@ -28,39 +27,143 @@ struct Args {
     connector: Option<String>,
 }
 
+struct DrmDisplay {
+    gbm_device: GbmDevice<Card>,
+    crtc: CrtcInfo,
+    connector: ConnectorInfo,
+    mode: Mode,
+    width: u32,
+    height: u32,
+}
+
+impl DrmDisplay {
+    fn init(args: &Args) -> Option<DrmDisplay> {
+        // TODO(bschwind) - Use libdrm to iterate over available DRM devices.
+        let gpu = Card::open(&args.card_path);
+        dbg!(gpu.get_driver().expect("Failed to get GPU driver info"));
+        dbg!(gpu.get_bus_id().expect("Failed to get GPU bus ID"));
+
+        let resources = gpu.resource_handles().expect("Failed to get GPU resource handles");
+
+        print_connector_info(&gpu, &resources);
+
+        let connector = {
+            let mut connectors = get_connected_connectors(&gpu);
+            let card_path = args.card_path.display();
+            if let Some(name) = &args.connector {
+                connectors.find(|info| &get_connector_name(info) == name).unwrap_or_else(|| {
+                    panic!(
+                    "Connector {name} does not exist or is not connected to {card_path}, exiting"
+                )
+                })
+            } else {
+                connectors
+                    .next()
+                    .unwrap_or_else(|| panic!("No connector connected to {card_path}, exiting."))
+            }
+        };
+
+        let connector_interface = connector.interface().as_str();
+        let interface_id = connector.interface_id();
+
+        println!("Using connector: {connector_interface}-{interface_id}");
+
+        let Some(mode) = connector_preferred_mode(&connector) else {
+            println!("No preferred mode for the selected connector, exiting");
+            return None;
+        };
+
+        println!("Using mode: {mode:?}");
+
+        let Some(encoder_handle) = first_encoder(&connector) else {
+            println!("Selected connector does not have an encoder, exiting");
+            return None;
+        };
+
+        let encoder =
+            gpu.get_encoder(encoder_handle).expect("Failed to get encoder from encoder handle");
+        dbg!(encoder);
+
+        let crtc_handle = *resources
+            .filter_crtcs(encoder.possible_crtcs())
+            .first()
+            .expect("No CRTCs found for encoder");
+        let crtc = gpu.get_crtc(crtc_handle).expect("Failed to get CRTC from CRTC handle");
+        dbg!(crtc);
+
+        let (width, height) = mode.size();
+        let (width, height) = (width as u32, height as u32);
+        let gbm_device = GbmDevice::new(gpu).expect("Failed to create GbmDevice");
+        Some(DrmDisplay { gbm_device, crtc, connector, mode, width, height })
+    }
+}
+
 struct Window {
-    gbm_device: *mut c_void,
+    gbm_surface: Surface<BufferObject<()>>,
+    drm_display: DrmDisplay,
 }
 
 impl Window {
-    fn new(gbm_device: *mut c_void) -> Window {
-        Window { gbm_device }
+    fn new(drm_display: DrmDisplay) -> Window {
+        let format = BufferFormat::Argb8888;
+        // NOTE(mbernat): This should be used with create_surface() on drivers that support it (not nvidia)
+        // let usage = BufferObjectFlags::SCANOUT | BufferObjectFlags::RENDERING;
+        let modifiers = std::iter::once(Modifier::Linear);
+        let gbm_surface: Surface<BufferObject<()>> = drm_display
+            .gbm_device
+            .create_surface_with_modifiers(drm_display.width, drm_display.height, format, modifiers)
+            .unwrap();
+
+        Window { gbm_surface, drm_display }
+    }
+
+    fn swap_buffers(&self) {
+        // TODO(mbernat): move this elsewhere
+        let depth_bits = 24;
+        let bits_per_pixel = 32;
+
+        // SAFETY: this must be called exactly once after `eglSwapBuffers`, which happens in `frame.finish()`.
+        let buffer_object = unsafe { self.gbm_surface.lock_front_buffer().unwrap() };
+        // TODO(mbernat): We should recycle framebuffers;
+        // one can store an FB handle in buffer object's user_data() and reuse it when it exists
+        let fb = self
+            .drm_display
+            .gbm_device
+            .add_framebuffer(&buffer_object, depth_bits, bits_per_pixel)
+            .unwrap();
+        self.drm_display
+            .gbm_device
+            .set_crtc(
+                self.drm_display.crtc.handle(),
+                Some(fb),
+                (0, 0),
+                &[self.drm_display.connector.handle()],
+                Some(self.drm_display.mode),
+            )
+            .unwrap();
+        // TODO(mbernat): Subsequent mode setting should be done with gbm_device.page_flip()
     }
 }
 
 mod rwh_impl {
     use super::Window;
+    use gbm::AsRaw;
     use raw_window_handle::*;
 
+    // SAFETY: surface is valid for the duration of the program
     unsafe impl HasRawWindowHandle for Window {
         fn raw_window_handle(&self) -> RawWindowHandle {
-            // alternative
-            // RawWindowHandle::Drm()
-
             let mut handle = GbmWindowHandle::empty();
-            handle.gbm_surface = std::ptr::null_mut();
-
+            handle.gbm_surface = self.gbm_surface.as_raw() as *mut _;
             RawWindowHandle::Gbm(handle)
         }
     }
 
+    // SAFETY: device is valid for the duration of the program
     unsafe impl HasRawDisplayHandle for Window {
         fn raw_display_handle(&self) -> RawDisplayHandle {
-            // alternative
-            // RawDisplayHandle::Drm();
-
             let mut handle = GbmDisplayHandle::empty();
-            handle.gbm_device = self.gbm_device;
+            handle.gbm_device = self.drm_display.gbm_device.as_raw() as *mut _;
             RawDisplayHandle::Gbm(handle)
         }
     }
@@ -68,87 +171,16 @@ mod rwh_impl {
 
 fn main() {
     let args = Args::parse();
+    let drm_display = DrmDisplay::init(&args).unwrap();
+    let window = Window::new(drm_display);
+    let glium_display = glutin::init(&window);
+    {
+        use glium::Surface;
+        let mut frame = glium_display.draw();
+        frame.clear_color(0.2, 0.0, 0.5, 1.0);
+        frame.finish().unwrap();
+        window.swap_buffers();
+    }
 
-    // TODO(bschwind) - Use libdrm to iterate over available DRM devices.
-    let gpu = Card::open(&args.card_path);
-    dbg!(gpu.get_driver().expect("Failed to get GPU driver info"));
-    dbg!(gpu.get_bus_id().expect("Failed to get GPU bus ID"));
-
-    let resources = gpu.resource_handles().expect("Failed to get GPU resource handles");
-
-    print_connector_info(&gpu, &resources);
-
-    let connector = {
-        let mut connectors = get_connected_connectors(&gpu);
-        let card_path = args.card_path.display();
-        if let Some(name) = &args.connector {
-            connectors.find(|info| &get_connector_name(info) == name).unwrap_or_else(|| {
-                panic!(
-                    "Connector {name} does not exist or is not connected to {card_path}, exiting"
-                )
-            })
-        } else {
-            connectors
-                .next()
-                .unwrap_or_else(|| panic!("No connector connected to {card_path}, exiting."))
-        }
-    };
-
-    let connector_interface = connector.interface().as_str();
-    let interface_id = connector.interface_id();
-
-    println!("Using connector: {connector_interface}-{interface_id}");
-
-    let Some(preferred_mode) = connector_preferred_mode(&connector) else {
-        println!("No preferred mode for the selected connector, exiting");
-        return;
-    };
-
-    println!("Using mode: {preferred_mode:?}");
-
-    let Some(encoder_handle) = first_encoder(&connector) else {
-        println!("Selected connector does not have an encoder, exiting");
-        return;
-    };
-
-    let encoder =
-        gpu.get_encoder(encoder_handle).expect("Failed to get encoder from encoder handle");
-    dbg!(encoder);
-
-    let crtc_handle = *resources
-        .filter_crtcs(encoder.possible_crtcs())
-        .first()
-        .expect("No CRTCs found for encoder");
-    let crtc = gpu.get_crtc(crtc_handle).expect("Failed to get CRTC from CRTC handle");
-    dbg!(crtc);
-
-    let (width, height) = preferred_mode.size();
-    let (width, height) = (width as u32, height as u32);
-
-    let gbm = GbmDevice::new(gpu).expect("Failed to create GbmDevice");
-    let window = Window::new(gbm::AsRaw::as_raw(&gbm) as *mut _);
-
-    let mut buffer_object = gbm
-        .create_buffer_object::<()>(
-            width,
-            height,
-            BufferFormat::Argb8888,
-            BufferObjectFlags::SCANOUT | BufferObjectFlags::WRITE,
-        )
-        .unwrap();
-
-    let buffer_data = vec![255u8; (width * height * 4) as usize];
-
-    buffer_object.write(&buffer_data).unwrap().unwrap();
-
-    let depth_bits = 32;
-    let bits_per_pixel = 32;
-    let fb = gbm.add_framebuffer(&buffer_object, depth_bits, bits_per_pixel).unwrap();
-
-    let _glutin_display = glutin::init(&window);
-
-    gbm.set_crtc(crtc_handle, Some(fb), (0, 0), &[connector.handle()], Some(preferred_mode))
-        .unwrap();
-
-    // std::thread::sleep(std::time::Duration::from_secs(5));
+    std::thread::sleep(std::time::Duration::from_secs(3));
 }
